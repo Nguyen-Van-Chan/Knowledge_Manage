2aa793a95a11299aae75163297c6fc22d8fdb7f5
Trang đăng khải <42131590+Nguyen-Van-Chan@users.noreply.github.com>
2022-10-11 11:12:56 +0700

    update

---
+++ KnowledgeManage/Models/Exercise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowledgeManage.Models
+{
+    //dangkhai
+    public class Exercise
+    {
+        [Key]
+        public int Id_Exercise { get; set; }
+        public string Link_Exercise { get; set; }
+        public string Name_Exercise { get; set; }
+        public string LessonId_Lesson { get; set; }
+
+        public virtual Lesson Lesson { get; set; }
+
+        public Exercise()
+        {
+
+        }
+    }
+}
